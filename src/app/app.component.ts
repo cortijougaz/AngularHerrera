dd5c42c523d5b284917dce018ab881eb0bcc084a
Ricardo Cortijo <cortijougaz@gmail.com>
2025-10-04 15:42:30 -0500

    Cambio en el tipo de estado de los elementos de app.component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Ricardo ';
-  lastName = 'Cortijo Ugaz';
-  age = 32;
+  public title: string = 'Ricardo ';
+  public lastName: string = 'Cortijo Ugaz';
+  public age: number = 32;
 }
