657ed74aadcd2fa41b2988127b9b573c8c225cc0
Ricardo Cortijo <cortijougaz@gmail.com>
2025-10-04 13:54:53 -0500

    Cambio de nomenclatura para el app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class App {
+export class AppComponent {
   title = 'Ricardo ';
   lastName = 'Cortijo Ugaz'
 }
