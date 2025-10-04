63aa3192bc8732aa8342bbf55144441d0be5a223
Ricardo Cortijo <cortijougaz@gmail.com>
2025-10-04 14:01:35 -0500

    Cambios menores.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Ricardo ';
-  lastName = 'Cortijo Ugaz'
+  title = 'Ricardo';
+  lastName = 'Cortijo'
 }
