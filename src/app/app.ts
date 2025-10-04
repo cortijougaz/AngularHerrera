4c29b4a0868520b53e774ae4b70bea26dc8f176d
Ricardo Cortijo <cortijougaz@gmail.com>
2025-10-04 13:35:37 -0500

    Configuraciones iniciales.

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('bases');
+  title = 'Ricardo ';
+  lastName = 'Cortijo Ugaz'
 }
