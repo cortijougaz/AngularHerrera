550fcd49cec4ed1a038cfcc06aaa19df78d7f7de
Ricardo Cortijo <cortijougaz@gmail.com>
2025-10-05 20:05:21 -0500

    02-Extender-Bases: Se agrega un nuevo personaje con las señales.

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -13,10 +13,32 @@ interface Character {
   styleUrls: ['./dragonball-page.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DragonballPageComponent{
+export class DragonballPageComponent {
+  name = signal('Gohan');
+  power = signal(100);
+
   characters = signal<Character[]>([
     {id: 1, name: 'Goku', power: 9001},
-    {id: 1, name: 'Vegeta', power: 8000},
-    {id: 1, name: 'Piccolo', power: 3000},
+    {id: 2, name: 'Vegeta', power: 8000},
+    {id: 3, name: 'Piccolo', power: 3000},
+    {id: 4, name: 'Yamcha', power: 500},
   ]);
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    }
+    this.characters.update((list: Character[]): Character[] => [...list, newCharacter]);
+    this.resetFields();
+  };
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
